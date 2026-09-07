9a8e17371fcad864d8ea3b46850b9122302a2abf
liptan <liptankumar465@gmail.com>
2026-09-08 00:00:43 +0530

    print() function

---
+++ Python basics/2_Print().py
@@ -0,0 +1,57 @@
+# Print():- Use for display output in screen
+
+"""----------Escaps sequences-----------"""
+# \" -> Double quote
+# \' -> Single quote
+# \\ -> Back slash
+# \n -> New line
+# \t -> Tab
+# \b -> Back space
+
+print("print(\"This is first code in python!\")")
+print("This is first code in python!")
+print('-' * 40)
+
+print("print('This is also print')")
+print('This is also print')
+print('-' * 40)
+
+print("print(\"Hi 'python'\")")
+print("Hi 'python'")
+print('-' * 40)
+
+print("print('Hi \"python\"')")
+print('Hi "python"')
+print('-' * 40)
+
+print("print(\"Hi \"python\"\")")
+print("Hi \"python\"")
+print('-' * 40)
+
+# print:- path:C:\users\liptan
+print("print(\"path:c:\\users\\liptan\")")
+print("path:c:\\users\\liptan")
+print('-' * 40)
+
+# '\n' -> add next line
+print("liptan and kumar print sepret line!")
+# 1 -> way
+print("liptan")
+print("kumar")
+print('-' * 40)
+# 2 -> way
+print("liptan\nkumar")
+print('-' * 40)
+
+# '\t' -> add tab
+print("liptan and kumar btw add tab line!")
+print("liptan\tkumar")
+print("\tliptan kumar")
+print("liptan\n\tkumar")
+print('-' * 40)
+
+# """ text """ for type in next line useing print()
+print("""your learning:
+\t-python
+\t-data science
+\t-AI""")
