661451c7422d55d5966201d5510e8b375cb1eeb1
liptan <liptankumar465@gmail.com>
2026-09-08 19:53:58 +0530

    variables

---
+++ Python basics/3_Variables.py
@@ -0,0 +1,38 @@
+"""Variables are containers that stored valuse in memory"""
+
+# In python variables are dynamic
+
+""" Key points:
+        1 -> make programs dynamic
+        2 -> name to store value
+        3 -> stored in memory
+        4 -> reusable
+        5 -> updatable
+"""
+
+age = 18
+name = "liptan"
+language = "python"
+
+print("my name is", name)
+print("my age is", age)
+print(name, "is learning", language, "language")
+print("-" * 30)
+
+x = 10
+y = x + 5
+print(y)
+print(x + y)
+print("-" * 30)
+
+is_tall = True
+print(is_tall)
+print("-" * 30)
+
+is_tall = False
+print(is_tall)
+print("-" * 30)
+
+nothing = None
+print(nothing)
+print("-" * 30)
