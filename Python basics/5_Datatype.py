1000ab389c628ae3d1e145972d00a0b1155035a2
liptan <liptankumar465@gmail.com>
2026-09-08 19:54:50 +0530

    data types in py

---
+++ Python basics/5_Datatype.py
@@ -0,0 +1,45 @@
+""" Data types in python """
+"""
+    -> In python basically 3 types of data
+    1-> No value
+        -> none
+
+    2-> Single value/primitive types
+        -> int, float, bool, str
+
+    3-> Multi valus/data structrue/collections/container
+        -> list, set, tuple, dict
+"""
+"""
+    Key points:
+        / python automatically detect data type
+        / Dynamic: data types can change any
+          time.
+"""
+
+a = 5  # int data type
+print(a)
+
+b = 3.14  # float data type
+print(b)
+
+c = 'Hi "python"'  # string data type
+print(c)
+
+d = True  # bool data type
+print(d)
+
+e = [1, 2, 3, 4, 5]  # list
+print(e)
+
+f = {1, 2, 3, 4, 5}  # set
+print(f)
+
+g = (1, 2, 3, 4, 5)  # tuple
+print(g)
+
+h = {'a': 1, 'b': 2, 'c': 3}  # dict
+print(h)
+
+i = None
+print(i)
