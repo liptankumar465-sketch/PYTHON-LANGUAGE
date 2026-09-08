951c6b023a5653b916c853f196aa56515e873a83
liptan <liptankumar465@gmail.com>
2026-09-08 19:55:20 +0530

    the diff btw func or methods

---
+++ Python basics/6_Func_or_Methods.py
@@ -0,0 +1,16 @@
+import math
+mes = "Hello"
+"""
+    Function :- Independent block of code.
+    Syntax -> function_name(value)
+"""
+print(mes)  # Hello
+print(len(mes))  # 5
+print(type(mes))  # str
+
+"""
+    Methods:- Function belong to objects/classes.
+    Syntax -> value.method_name()
+"""
+print(mes.upper()) # HELLO
+print(mes.lower()) # hello
