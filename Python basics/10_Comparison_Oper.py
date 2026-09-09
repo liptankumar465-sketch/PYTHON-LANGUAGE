082ed44e3fc368885ff39a4ead3c8d3c683cbc9d
liptan <liptankumar465@gmail.com>
2026-09-09 22:57:46 +0530

    compairson operators

---
+++ Python basics/10_Comparison_Oper.py
@@ -0,0 +1,25 @@
+# Comparison Operators
+# Syntax:- val1 comOper val2 => True/False
+
+# Numbers
+print(10 == 10)
+print(10 != 10)
+print(7 > 3)
+print(7 >= 3)
+print(3 < 7)
+print(7 <= 7)
+
+# strings
+print("a" == "A")
+print("a" != "A")
+print("a" == "a")
+print("a" != "a")
+print("a" < "b")
+
+# Chaining rule
+print(1 < 5 < 10)
+print(1 < 10 < 5)
+
+# Is age btw 18 and 30?
+age = 18
+print(18 <= age <= 30)
