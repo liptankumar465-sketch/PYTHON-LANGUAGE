95bbf57f8105b4fad7e63bb049543c2a999b8500
liptan <liptankumar465@gmail.com>
2026-09-09 08:06:24 +0530

    data types in py

--- Python basics/5_Datatype.py
+++ Python basics/5_Datatype.py
@@ -17,29 +17,53 @@
           time.
 """
 
-a = 5  # int data type
-print(a)
-
-b = 3.14  # float data type
-print(b)
-
-c = 'Hi "python"'  # string data type
-print(c)
-
-d = True  # bool data type
-print(d)
-
+a = 5  # int
+b = 3.14  # float
+c = 'Hi python'  # str
+c2 = "Hi python"  # str
+c3 = "12345"  # str
+d = True  # bool
+d2 = False  # bool
+i = None  # none
 e = [1, 2, 3, 4, 5]  # list
-print(e)
-
 f = {1, 2, 3, 4, 5}  # set
-print(f)
-
 g = (1, 2, 3, 4, 5)  # tuple
-print(g)
-
 h = {'a': 1, 'b': 2, 'c': 3}  # dict
-print(h)
 
-i = None
-print(i)
+
+text = "good"
+number = 50
+# type() -> works for both
+print(type(text))
+print(type(number))
+
+# len() -> only works for string
+print(len(text))
+# print(len(number)) -> this error passed
+
+# bit_length() -> only works for integer or float
+# print(text.bit_length()) -> this is error pass
+print(number.bit_length())
+
+# -------------challenge-----------------#
+age = 18
+hight = 5.4
+name = "liptan"
+is_student = True
+job = None
+
+print(age)
+print(type(age))
+print(len(str(age)))
+print(hight)
+print(type(hight))
+print(len(str(hight)))
+print(name)
+print(type(name))
+print(len(name))
+print(is_student)
+print(type(is_student))
+print(len(str(is_student)))
+print(job)
+print(type(job))
+print(len(str(job)))
