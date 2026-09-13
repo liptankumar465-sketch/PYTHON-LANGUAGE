678821683614bbbd019109df9a77c434ae31bc98
liptan <liptankumar465@gmail.com>
2026-09-13 23:57:19 +0530

    Data str

---
+++ Python basics/21_Data_Structure.py
@@ -0,0 +1,23 @@
+
+#!----------------Data Structure------------------#
+# ? Is a way of organizing and storing data so it can
+# ? be used efficiently.
+# todo: 1 -> list[] --> (Common)
+# todo: 2 -> tuple() --> (No changes!)
+# todo: 3 -> set{} --> (unique!)
+# todo: 4 -> dict{} --> (key : value)
+
+
+#! List properties:
+#? Ordered collection of items.
+#? Changeble.
+#? Allows duplicates.
+
+
+
+
+
+
+
+
+
