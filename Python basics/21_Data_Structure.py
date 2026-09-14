91d46d8effd5589a8bdfc2dd7eb5c6ebacf89eb0
liptan <liptankumar465@gmail.com>
2026-09-14 23:18:52 +0530

    Data str:- How to create a list

--- Python basics/21_Data_Structure.py
+++ Python basics/21_Data_Structure.py
@@ -9,15 +9,46 @@
 
 
 #! List properties:
-#? Ordered collection of items.
-#? Changeble.
-#? Allows duplicates.
+# ? Ordered collection of items.
+# ? Changeble.
+# ? Allows duplicates.
 
+#todo: HOW TO CREATE A LIST.
+# ? Creates list manually:
+empty = []
+print(empty)
+print(type(empty))
 
+letters = ['a', 'b', 'c']
+print(letters)
+print(type(letters))
 
+numbers = [1, 2, 3]
+print(numbers)
+print(type(numbers))
 
+mixed = [1, 'a', True, None]
+print(mixed)
+print(type(mixed))
 
+# ? Create lists by list():
+# todo: list(value) converts an iterable (sequence)
+# todo: into a list.
 
+empty = list()
+print(empty)
 
+letters = list('Python')
+print(letters)
 
+numbers = list(range(1, 6))
+print(numbers)
 
+# ? Create a 2-D list:
+matrix = [[1, 2, 3],
+          [4, 5, 6]]
+print(matrix)
+
+mixed_matrix = [['a', 'b', 'c'],
+                [1, 2, 3]]
+print(mixed_matrix)
